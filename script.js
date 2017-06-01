72bc3b99786d7f3fa858dbec6eb4a1104ef0f3a3
iradkot <irad16@gmail.com>
2017-06-01 13:22:04 +0300

    added sort by category and name

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var beerList = [];
-var upOrDown = 0;
+var sortFlagName = 0;
+var sortFlagCategory = 0;
+var sortFlagReview = 0;
 $('.post-beer').click(function() {
     addBeer($('.beer-input').val(), $('.category-input').val(), $('.review-select').val());
     $('.beer-input').val('');
@@ -7,8 +9,19 @@ $('.post-beer').click(function() {
     $('.review-select').val('');
     updateBeers();
 });
-$('.sort-beer').click(function() {
-	upOrDown === 0? sortBeersFromDown(): sortBeersFromUp();
+$('.sort-name').click(function() {
+	sortFlagName === 0? sortBeersFromDown('name'): sortBeersFromUp('name');
+	sortFlagName === 0? sortFlagName=1: sortFlagName=0;
+    updateBeers()
+});
+$('.sort-category').click(function() {
+	sortFlagCategory === 0? sortBeersFromDown('category'): sortBeersFromUp('category');
+	sortFlagCategory === 0? sortFlagCategory=1: sortFlagCategory=0;
+    updateBeers()
+});
+$('.sort-review').click(function() {
+	sortFlagReview === 0? sortBeersFromDown('review'): sortBeersFromUp('review');
+	sortFlagReview === 0? sortFlagReview=1: sortFlagReview=0;
     updateBeers()
 });
 
@@ -24,12 +37,12 @@ function updateBeers() {
     }
 }
 
-function sortBeersFromDown() {
+function sortBeersFromDown(property, flag) {
     var count = 0;
     while (count < beerList.length) {
         count = 1;
         for (var i = 0; i < beerList.length - 1; i++) {
-            if (beerList[i].review > beerList[i + 1].review) {
+            if (beerList[i][property] > beerList[i + 1][property]) {
                 count = 0;
                 beerList.splice(i + 2, 0, beerList[i]);
                 beerList.splice(i, 1);
@@ -38,15 +51,14 @@ function sortBeersFromDown() {
             }
         }
     }
-    upOrDown=1;
     $('.beers-list').empty();
 }
-function sortBeersFromUp() {
+function sortBeersFromUp(property) {
     var count = 0;
     while (count < beerList.length) {
         count = 1;
         for (var i = 0; i < beerList.length - 1; i++) {
-            if (beerList[i].review < beerList[i + 1].review) {
+            if (beerList[i][property] < beerList[i + 1][property]) {
                 count = 0;
                 beerList.splice(i, 2, beerList[i+1], beerList[i]);
             } else {
@@ -54,6 +66,5 @@ function sortBeersFromUp() {
             }
         }
     }
-    upOrDown=0;
     $('.beers-list').empty();
 }
